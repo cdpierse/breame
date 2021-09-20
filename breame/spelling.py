1d23ee2a5ac1739ff3c66a6e73db7bb200f21bad
Charles Pierse <charlespierse@gmail.com>
2021-09-20 15:48:32 +0100

    Use word processing util in spelling.py

--- breame/spelling.py
+++ breame/spelling.py
@@ -2,13 +2,14 @@ from breame.data.spelling_constants import (
     AMERICAN_ENGLISH_SPELLINGS,
     BRITISH_ENGLISH_SPELLINGS,
 )
+from breame.utils import process_word
 
 
 def get_british_spelling(word: str) -> str:
     """
     Get the British spelling of an American English word.
     """
-    word = word.lower().strip()
+    word = process_word(word)
     if word not in AMERICAN_ENGLISH_SPELLINGS:
         return word
     return AMERICAN_ENGLISH_SPELLINGS[word]
@@ -18,7 +19,7 @@ def get_american_spelling(word: str) -> str:
     """
     Get the American spelling of a British English word.
     """
-    word = word.lower().strip()
+    word = process_word(word)
     if word not in BRITISH_ENGLISH_SPELLINGS:
         return word
     return BRITISH_ENGLISH_SPELLINGS[word]
@@ -28,7 +29,7 @@ def american_spelling_exists(word: str) -> bool:
     """
     Check if an American English spelling exists.
     """
-    word = word.lower().strip()
+    word = process_word(word)
     return word in AMERICAN_ENGLISH_SPELLINGS
 
 
@@ -36,5 +37,5 @@ def british_spelling_exists(word: str) -> bool:
     """
     Check if a British English spelling exists.
     """
-    word = word.lower().strip()
+    word = process_word(word)
     return word in BRITISH_ENGLISH_SPELLINGS
